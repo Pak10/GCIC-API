e5aa815ec0e621a655c1cc6039818469dcc2b155
Pak10 <kwesigapeteralbert@gmail.com>
2026-01-03 19:08:28 +0300

    user name

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -92,14 +92,14 @@ class TransactionController extends Controller
 
             $accounts = Account::where('user_id', $user->id)->pluck('id');
 
-            $accountTransactions = AccountTransaction::with(['account'])->whereIn('account_id', $accounts)->orderBy('created_at', 'desc');
+            $accountTransactions = AccountTransaction::with(['account.user'])->whereIn('account_id', $accounts)->orderBy('created_at', 'desc');
 
         }
         else{
 
             if($user->can('view-transactions')){
 
-                $accountTransactions = AccountTransaction::with(['account'])->orderBy('created_at', 'desc');
+                $accountTransactions = AccountTransaction::with(['account.user'])->orderBy('created_at', 'desc');
             }
             else{
         
